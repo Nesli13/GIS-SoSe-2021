79c377da107e44f57127b8894e16c1e588a93d4d
Neslisah Koc <neslisah.koc@hs-furtwangen.de>
2021-04-28 15:29:04 +0200

    Aufgabe2_mit Beispielen

---
+++ Aufgabe2/script.ts
@@ -0,0 +1,23 @@
+console.log("Jellooo");
+//console.log(true + true);
+console.log(5 + 1);
+console.log(5 + "1");
+console.log("5" + 1);
+//console.log('5' - 1);
+
+//BONUS:
+console.log(('b' + 'a' + + 'a' + 'a').toLowerCase());
+// '' macht das gleiche wie "" 
+//Strings können nicht multipliziert, dividiert oder subtrahiert werden!
+
+console.log(3 > 2); // -> true
+console.log(3 < 2); // -> false
+console.log("Aal" < "Zebra"); // -> true
+
+//console.log(0 == false); // -> true 
+//console.log("" == false); // -> true 
+console.log(false == false); // -> true 
+
+//console.log(0 === false); // -> false
+//console.log("" === false); // -> false 
+console.log(false === false); // -> true 
