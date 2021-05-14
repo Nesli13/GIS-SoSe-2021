2a5286f4c507e751a93be47e959dac60832698ca
Neslisah Koc <neslisah.koc@hs-furtwangen.de>
2021-05-14 10:14:24 +0200

    Leggo

--- Aufgabe2/script.js
+++ Aufgabe2/script.js
@@ -18,4 +18,19 @@ console.log(false == false); // -> true
 //console.log(0 === false); // -> false
 //console.log("" === false); // -> false 
 console.log(false === false); // -> true 
+let five = 5;
+console.log(five * five);
+let weather = "sunny";
+console.log(weather);
+weather = "dark";
+console.log(weather);
+let lukasDebt = 1000;
+lukasDebt = lukasDebt - 50;
+console.log(lukasDebt);
+let one = 1, two = 2;
+console.log(one + two);
+//let name: string = "Lukas";
+const greeting = "Hello ";
+console.log(greeting + name);
+prompt("Passwort eingeben");
 //# sourceMappingURL=script.js.map
--- Aufgabe2/script.ts
+++ Aufgabe2/script.ts
@@ -21,3 +21,24 @@ console.log(false == false); // -> true
 //console.log(0 === false); // -> false
 //console.log("" === false); // -> false 
 console.log(false === false); // -> true 
+
+let five: number = 5;
+console.log(five * five);
+
+let weather: string = "sunny";
+console.log(weather);
+weather = "dark";
+console.log(weather);
+
+let lukasDebt: number = 1000;
+lukasDebt = lukasDebt - 50;
+console.log(lukasDebt);
+
+let one: number = 1, two: number = 2;
+console.log(one + two);
+
+//let name: string = "Lukas";
+const greeting: string = "Hello ";
+console.log(greeting + name);
+
+prompt("Passwort eingeben");
